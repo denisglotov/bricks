2f0c2709128891ab54ede85b088a1dfb6059a572
Denis Glotov <denis@glotov.org>
2020-11-01 22:09:42 +0200

    Basic multithreaded version on asyncs

--- task.cxx
+++ task.cxx
@@ -1,6 +1,7 @@
 #include <cassert>
 #include <cctype>
 #include <fstream>
+#include <future>
 #include <iostream>
 #include <map>
 #include <numeric>
@@ -36,26 +37,68 @@ string decode(CellId cell) {
     return string(1, (cell >> 24) + 'A') + to_string(cell & 0xFFFFFF);
 }
 
+template <typename T>
+class ProducerConsumerQueue {
+    deque<T> que;
+    mutex mu;
+    condition_variable cv;
+
+public:
+    void enqueue(T id) {
+        unique_lock<mutex> lock(mu);
+        que.push_back(id);
+        cv.notify_one();
+    }
+
+    deque<T> dequeue() {
+        unique_lock<mutex> lock(mu);
+        while (que.empty()) cv.wait(lock);
+        deque<T> res;
+        res.swap(que);
+        return res;
+    }
+};
+
+
+// Common space for worker threads.
+struct Context {
+    ProducerConsumerQueue<CellId> ready_cells;
+    int stat_total_jobs;
+    unordered_set<thread::id> stat_job_ids;
+    mutex stat_mu;
+
+    void add_stat() {
+        unique_lock<mutex> stat_mu;
+        ++stat_total_jobs;
+        stat_job_ids.insert(this_thread::get_id());
+    }
+};
+
+
+// Data for every cell.
 struct CellData {
     int ind;
     int value;
     int epoch;
     vector<int> args;
+    int remote_result;
 
     CellData() : ind(0), value(0), epoch(0) {}
 
-    int eval() {
-        int result = accumulate(args.begin(), args.end(), 0);
-        args.clear();
-        return result;
+    void eval(vector<int> local_args, CellId id, Context *ctx) {
+        remote_result = accumulate(local_args.begin(), local_args.end(), 0);
+        ctx->add_stat();
+        ctx->ready_cells.enqueue(id);
     }
 };
+
 
 
 int main(int argc, char *argv[]) {
     unordered_map<CellId, CellData> values;
     unordered_map<CellId, unordered_set<CellId>> deps;
 
+    // Parse input file.
     ifstream input(argc > 1? argv[1] : "input.txt");
     string str;
     while (getline(input, str)) {
@@ -88,31 +131,51 @@ int main(int argc, char *argv[]) {
     }
 
     // Evaluate cells by sorting them.
+    int jobs = 0;
+    Context ctx;
     queue<CellId> que;
     for (auto &data: values) if (data.second.ind == 0) que.push(data.first);
 
-    while (!que.empty()) {
-        CellId cell = que.front();
-        que.pop();
-        for (CellId d: deps[cell]) {
-            CellData &data = values[d];
-            data.args.push_back(values[cell].value);
-            if (--data.ind == 0) {
-                data.value = data.eval();
-                que.push(d);
+    while (true) {
+        while (!que.empty()) {
+            CellId cell = que.front();
+            que.pop();
+            for (CellId dep: deps[cell]) {
+                CellData &data = values[dep];
+                data.args.push_back(values[cell].value);
+                if (--data.ind == 0) {
+                    ++jobs;
+                    (void)async(launch::async, &CellData::eval, &data, data.args, dep, &ctx);
+                }
             }
+            deps.erase(cell);
         }
-        deps.erase(cell);
-    }
+        if (!jobs) break;
+        for (CellId cell: ctx.ready_cells.dequeue()) {
+            --jobs;
+            CellData &data = values[cell];
+            // TODO: if (data.epoch)
+            data.value = data.remote_result;
+            que.push(cell);
+        }
+    };
+
     if (deps.size()) {
         cerr << "Warning: the following are unresolved: ";
         for (auto &d: deps) cerr << decode(d.first).c_str() << " ";
         cerr << endl;
     }
 
+    // Print results.
     for (auto &val: map<CellId, CellData>(values.begin(), values.end())) {
         if (val.second.ind == 0) cout << decode(val.first).c_str() << " = " << val.second.value << "\n";
     }
+    cerr << "Total jobs executed: " << ctx.stat_total_jobs
+         << ", current thread: " << this_thread::get_id() << ".\n"
+         << "Threads used: ";
+    for (auto t: ctx.stat_job_ids) cerr << t << " ";
+    cerr << "\n";
+
     // for (auto &dep: deps) {
     //     cout << decode(dep.first).c_str() << ": ";
     //     for (auto cell: dep.second) cout << decode(cell).c_str() << " ";
