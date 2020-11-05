4635237d6dab72d4b1c30b828ba9819394d8f1a3
Denis Glotov <denis@glotov.org>
2020-11-05 18:26:50 +0200

    Add comments, readme.md, names brush-up

---
+++ readme.txt
@@ -0,0 +1,35 @@
+Spreadsheet calculation Demo
+============================
+
+Build with `g++ -x c++ -std=gnu++17 -O2 -Wall -pedantic -pthread task.cxx`.
+
+
+Restrictions
+------------
+
+1. Cell IDs must be: [A-Z][0-9]+, the number [0..15777215].
+
+2. Every non-empty line is either:
+   * `<cellId> = <number>` or
+   * `<cellId> = <cellId> [+ <cellId>[ + <cellId>[ ... ]]]`.
+
+3. No error handling or exception handling.
+
+
+Improvements
+------------
+
+1. Consider using lock-free alternatives for producer-consumer queue. But note
+happens-before relations: `CellData::remote_result` set by worker thread must
+become visible to the main thread after this cell is put to `ready_queue`.
+
+2. Use more robust thread pool with thread-local task queues.
+
+
+TODO
+----
+
+1. Check with thread sanitizer.
+2. Simulate time delays.
+3. Check function<void()>&&.
+4. data.args.swap()
--- task.cxx
+++ task.cxx
@@ -15,7 +15,6 @@ using namespace std;
 using CellId = uint32_t;
 
 
-// Parse the string `str` beginning with position `right`.
 bool parse(const string &str, size_t &left, size_t &right) {
     left = right;
     while (left != str.size() && !isalnum(str[left])) ++left;
@@ -46,6 +45,7 @@ class ProducerConsumerQueue {
     condition_variable cv;
 
 public:
+    // Worker enqueues id of its calculated cell.
     void enqueue(T id) {
         {
             unique_lock<mutex> lock(mu);
@@ -54,12 +54,11 @@ public:
         cv.notify_one();
     }
 
+    // Main thread dequeues array of calculated cell ids.
     deque<T> dequeue() {
         unique_lock<mutex> lock(mu);
-        while (que.empty()) cv.wait(lock);
-        deque<T> res;
-        res.swap(que);
-        return res;
+        cv.wait(lock, [this] { return !que.empty(); });
+        return move(que);
     }
 };
 
@@ -92,7 +91,7 @@ public:
             task();
         }
     }
-    
+
     void enqueue(function<void()>&& task) {
         {
             unique_lock<mutex> lock(mu);
@@ -114,12 +113,30 @@ public:
 };
 
 
+// Data for every cell on a page.
+struct CellData {
+    int value;          // current value
+    int indir;          // indirection - on how many cells does this one depend
+    int epoch;          // epoch of this cell, updated when user rewrites it
+    vector<int> args;   // arguments needed to calculate this cell
+    int remote_result;  // result of calculation by worker thread
+
+    CellData() : value(0), indir(0), epoch(0) {}
+};
+
+
 // Common space for worker threads.
-struct Context {
+struct WorkersContext {
     ProducerConsumerQueue<CellId> ready_cells;
     int stat_total_jobs;
     unordered_set<thread::id> stat_job_ids;
     mutex stat_mu;
+
+    void eval(const vector<int> &local_args, CellId id, CellData &data) {
+        data.remote_result = accumulate(local_args.begin(), local_args.end(), 0);
+        add_stat();
+        ready_cells.enqueue(id);
+    }
 
     void add_stat() {
         unique_lock<mutex> lock(stat_mu);
@@ -129,27 +146,9 @@ struct Context {
 };
 
 
-// Data for every cell.
-struct CellData {
-    int ind;
-    int value;
-    int epoch;
-    vector<int> args;
-    int remote_result;
-
-    CellData() : ind(0), value(0), epoch(0) {}
-
-    void eval(vector<int> local_args, CellId id, Context *ctx) {
-        remote_result = accumulate(local_args.begin(), local_args.end(), 0);
-        ctx->add_stat();
-        ctx->ready_cells.enqueue(id);
-    }
-};
-
-
 int main(int argc, char *argv[]) {
-    unordered_map<CellId, CellData> values;
-    unordered_map<CellId, unordered_set<CellId>> deps;
+    unordered_map<CellId, CellData> page;               // all cell data on this page
+    unordered_map<CellId, unordered_set<CellId>> deps;  // set of cells that depends on this one
 
     // Parse input file.
     ifstream input(argc > 1? argv[1] : "input.txt");
@@ -158,56 +157,56 @@ int main(int argc, char *argv[]) {
         size_t left = 0, right = 0;
         bool ok = parse(str, left, right);
         if (!ok) continue;
-        CellId lval = encode(str, left);
-        if (values.count(lval)) {
-            cerr << "Warning: " << decode(lval).c_str() << " is redefined.\n";
+        CellId left_cell = encode(str, left);
+        if (page.count(left_cell)) {
+            cerr << "Warning: " << decode(left_cell).c_str() << " is redefined.\n";
         }
 
         ok = parse(str, left, right);
         assert(ok);
         if (isCell(str, left)) {
-            int ind = 0;
+            int indir = 0;
             do {
-                ++ind;
-                CellId rval = encode(str, left);
-                if (deps[lval].count(rval)) {
-                    cerr << "Warning: " << decode(lval).c_str()
-                         << " already depends on " << decode(rval).c_str()
+                ++indir;
+                CellId right_cell = encode(str, left);
+                if (deps[left_cell].count(right_cell)) {
+                    cerr << "Warning: " << decode(left_cell).c_str()
+                         << " already depends on " << decode(right_cell).c_str()
                          << ", parsing '" << str.c_str() << "', ignored." << endl;
                 }
-                deps[rval].insert(lval);
+                deps[right_cell].insert(left_cell);
             } while (parse(str, left, right));
-            values[lval].ind = ind;
+            page[left_cell].indir = indir;
         } else {
-            values[lval].value = atoi(str.c_str() + left);
+            page[left_cell].value = atoi(str.c_str() + left);
         }
     }
 
     // Evaluate cells by sorting them.
     ThreadPool pool(thread::hardware_concurrency());  // -1
-    int jobs = 0;
-    Context ctx;
+    int running_jobs = 0;
+    WorkersContext ctx;
     queue<CellId> que;
-    for (auto &data: values) if (data.second.ind == 0) que.push(data.first);
+    for (auto &data: page) if (data.second.indir == 0) que.push(data.first);
 
     while (true) {
         while (!que.empty()) {
             CellId cell = que.front();
             que.pop();
-            for (CellId dep: deps[cell]) {
-                CellData &data = values[dep];
-                data.args.push_back(values[cell].value);
-                if (--data.ind == 0) {
-                    ++jobs;
-                    pool.enqueue([dep, &data, &ctx] { data.eval(data.args, dep, &ctx); });
+            for (CellId dcell: deps[cell]) {
+                CellData &data = page[dcell];
+                data.args.push_back(page[cell].value);
+                if (--data.indir == 0) {
+                    ++running_jobs;
+                    pool.enqueue([dcell, args = move(data.args), &data, &ctx] { ctx.eval(args, dcell, data); });
                 }
             }
             deps.erase(cell);
         }
-        if (!jobs) break;
+        if (!running_jobs) break;
         for (CellId cell: ctx.ready_cells.dequeue()) {
-            --jobs;
-            CellData &data = values[cell];
+            --running_jobs;
+            CellData &data = page[cell];
             // TODO: if (data.epoch)
             data.value = data.remote_result;
             que.push(cell);
@@ -215,17 +214,17 @@ int main(int argc, char *argv[]) {
     };
 
     if (deps.size()) {
-        cerr << "Warning: the following are unresolved: ";
+        cerr << "Warning: the following cells are left unresolved: ";
         for (auto &d: deps) cerr << decode(d.first).c_str() << " ";
         cerr << endl;
     }
 
     // Print results.
-    for (auto &val: map<CellId, CellData>(values.begin(), values.end())) {
-        if (val.second.ind == 0) cout << decode(val.first).c_str() << " = " << val.second.value << "\n";
+    for (auto &val: map<CellId, CellData>(page.begin(), page.end())) {
+        if (val.second.indir == 0) cout << decode(val.first).c_str() << " = " << val.second.value << "\n";
     }
     cerr << "Total jobs executed: " << ctx.stat_total_jobs
-         << ", current thread: " << this_thread::get_id() << ".\n"
+         << ", current thread id: " << this_thread::get_id() << ".\n"
          << "Threads used: ";
     for (auto t: ctx.stat_job_ids) cerr << t << " ";
     cerr << "\n";
